ec985510727bfe45a5f4a137076089edff215c46
Daniil Timizhev <ditimizhev@edu.hse.ru>
2024-01-21 21:53:25 +0300

    clear main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,3 @@
-#include "types/matrix/matrix.h"
-#include <iostream>
-
-using namespace matrix_lib;
-
 int main() {
-    auto matrix = Matrix<>::Diagonal({1, 2, 3, 4, 5});
-    std::cout << matrix / 3 << std::endl;
     return 0;
 }
