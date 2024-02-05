8ea899d85dcd1891b38a4992f16a5aee5a2bce9c
Daniil Timizhev <ditimizhev@edu.hse.ru>
2024-02-06 01:16:00 +0300

    refactor householder reflections

--- algorithms/householder.h
+++ algorithms/householder.h
@@ -3,6 +3,8 @@
 #include "../types/matrix/matrix.h"
 
 namespace matrix_lib::algorithms {
+using IndexType = std::size_t;
+
 template <utils::FloatOrComplex T>
 void HouseholderReduction(Matrix<T> &vector) {
     vector(0, 0) -= vector.GetEuclideanNorm();
@@ -10,16 +12,29 @@ void HouseholderReduction(Matrix<T> &vector) {
 }
 
 template <utils::FloatOrComplex T>
-void HouseholderLeftReflection(Matrix<T> &matrix) {
-    auto r_vector = matrix.GetColumn(0);
-    HouseholderReduction(r_vector);
-    matrix -= T{2} * (r_vector * Matrix<T>::Transposed(r_vector)) * matrix;
+void HouseholderLeftReflection(Matrix<T> &matrix, const Matrix<T> &vec,
+                               IndexType row = 0, IndexType c_from = 0,
+                               IndexType c_to = -1) {
+    if (c_to == -1) {
+        c_to = matrix.Columns();
+    }
+
+    Matrix<T> sub = matrix.GetSubmatrix(row, row + vec.Rows(), c_from, c_to);
+    sub -= (T{2} * vec) * (Matrix<T>::Transposed(vec) * sub);
+    matrix.AssignSubmatrix(sub, row, c_from);
 }
 
 template <utils::FloatOrComplex T>
-void HouseholderRightReflection(Matrix<T> &matrix) {
-    auto r_vector = matrix.GetColumn(matrix.Columns() - 1);
-    HouseholderReduction(r_vector);
-    matrix -= T{2} * matrix * (r_vector * Matrix<T>::Transposed(r_vector));
+void HouseholderRightReflection(Matrix<T> &matrix, const Matrix<T> &vec,
+                                IndexType col = 0, IndexType r_from = 0,
+                                IndexType r_to = -1) {
+    if (r_to == -1) {
+        r_to = matrix.Rows();
+    }
+
+    Matrix<T> sub =
+        matrix.GetSubmatrix(r_from, r_to, col, col + matrix.Columns());
+    sub -= (sub * Matrix<T>::Transposed(vec)) * (T{2} * vec);
+    matrix.AssignSubmatrix(sub, r_from, col);
 }
 } // namespace matrix_lib::algorithms
