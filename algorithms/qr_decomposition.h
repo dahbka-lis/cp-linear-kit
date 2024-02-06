b6af2e273bf0e91761b841b27c641aacc28f1173
Daniil Timizhev <ditimizhev@edu.hse.ru>
2024-02-06 15:31:56 +0300

    fix square matrix problem

--- algorithms/qr_decomposition.h
+++ algorithms/qr_decomposition.h
@@ -14,21 +14,19 @@ struct PairQR {
 
 template <utils::FloatOrComplex T>
 PairQR<T> HouseholderQR(const Matrix<T> &matrix) {
-    assert(matrix.Rows() == matrix.Columns() &&
-           "Householder QR Decomposition for square matrices.");
-
+    assert(matrix.Rows() >= matrix.Columns() &&
+           "Transpose the matrix for decompose it to QR.");
     PairQR<T> pair = {Matrix<T>::Identity(matrix.Rows()), matrix};
 
-    for (IndexType col = 0; col < matrix.Rows() - 1; ++col) {
-        Matrix<T> vec =
-            pair.R.GetColumn(col).GetSubmatrix(col, pair.R.Rows(), 0, 1);
+    for (IndexType col = 0; col < matrix.Columns(); ++col) {
+        Matrix<T> vec = pair.R.GetSubmatrix(col, pair.R.Rows(), col, col + 1);
         HouseholderReduction(vec);
 
         HouseholderLeftReflection(pair.R, vec, col, col);
         HouseholderLeftReflection(pair.Q, vec, col);
     }
 
-    pair.Q.Conjugate();
+    pair.Q.Transpose();
     return pair;
 }
 } // namespace matrix_lib::algorithms
