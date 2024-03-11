8ca03d4028ffdbfa6e04a2aa0b64e9bd2758faf4
Daniil Timizhev <ditimizhev@edu.hse.ru>
2024-03-11 11:58:48 +0300

    return basises of bidiag form

--- algorithms/bidiagonalization.h
+++ algorithms/bidiagonalization.h
@@ -6,30 +6,37 @@ namespace matrix_lib::algorithms {
 using IndexType = std::size_t;
 
 template <utils::FloatOrComplex T>
-void ToBidiagonalForm(Matrix<T> &matrix) {
-    for (IndexType col = 0; col < std::min(matrix.Rows(), matrix.Columns());
-         ++col) {
-        auto col_reduction =
-            matrix.GetSubmatrix(col, matrix.Rows(), col, col + 1).Copy();
-
-        HouseholderReduction(col_reduction);
-        HouseholderLeftReflection(matrix, col_reduction, col, col);
-
-        if (col + 2 < matrix.Columns()) {
-            auto row_reduction =
-                matrix.GetSubmatrix(col, col + 1, col + 1, matrix.Columns())
-                    .Copy();
-
-            HouseholderReduction(row_reduction);
-            HouseholderRightReflection(matrix, row_reduction, col + 1, col);
-        }
-    }
-}
+struct BidiagonalBasis {
+    Matrix<T> U;
+    Matrix<T> B;
+    Matrix<T> VT;
+};
 
 template <utils::FloatOrComplex T>
-Matrix<T> GetBidiagonalForm(const Matrix<T> &matrix) {
-    auto result = matrix;
-    ToBidiagonalForm(result);
-    return result;
+BidiagonalBasis<T> Bidiagonalize(const Matrix<T> &matrix) {
+    Matrix<T> B = matrix;
+    Matrix<T> U = Matrix<T>::Identity(B.Rows());
+    Matrix<T> V = Matrix<T>::Identity(B.Columns());
+
+    for (IndexType col = 0; col < std::min(B.Rows(), B.Columns()); ++col) {
+        auto col_reduction = B.GetSubmatrix(col, B.Rows(), col, col + 1).Copy();
+        HouseholderReduction(col_reduction);
+
+        HouseholderLeftReflection(B, col_reduction, col, col);
+        HouseholderLeftReflection(U, col_reduction, col);
+
+        if (col + 2 < B.Columns()) {
+            auto row_reduction =
+                B.GetSubmatrix(col, col + 1, col + 1, B.Columns()).Copy();
+            HouseholderReduction(row_reduction);
+
+            HouseholderRightReflection(B, row_reduction, col + 1, col);
+            HouseholderRightReflection(V, row_reduction, col + 1);
+        }
+    }
+
+    U.Conjugate();
+    V.Conjugate();
+    return {U, B, V};
 }
 } // namespace matrix_lib::algorithms
