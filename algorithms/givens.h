f41e8f7d53c1869c2a531ef5d46b2876005b9f53
Daniil Timizhev <ditimizhev@edu.hse.ru>
2024-04-15 19:56:45 +0300

    fix right rotation

--- algorithms/givens.h
+++ algorithms/givens.h
@@ -22,45 +22,45 @@ GivensPair<T> GetGivensCoefficients(T first_elem, T second_elem) {
     return {first_elem / sqrt_abs, -second_elem / sqrt_abs};
 }
 
-template <utils::FloatOrComplex T = long double>
-void GivensLeftRotation(Matrix<T> &matrix, IndexType from, IndexType to,
-                        T first, T second) {
+template <utils::MutableMatrixType M>
+void GivensLeftRotation(M &matrix, IndexType f_row, IndexType s_row,
+                        typename M::ElemType first, typename M::ElemType second) {
+    using T = typename M::ElemType;
     auto [cos, sin] = GetGivensCoefficients(first, second);
 
     for (IndexType i = 0; i < matrix.Columns(); ++i) {
-        auto cp_from = matrix(from, i);
-        auto cp_to = matrix(to, i);
+        auto cp_from = matrix(f_row, i);
+        auto cp_to = matrix(s_row, i);
 
         if constexpr (utils::details::IsFloatComplexT<T>::value) {
-            matrix(from, i) = std::conj(cos) * cp_from - std::conj(sin) * cp_to;
+            matrix(f_row, i) =
+                std::conj(cos) * cp_from - std::conj(sin) * cp_to;
         } else {
-            matrix(from, i) = cos * cp_from - sin * cp_to;
+            matrix(f_row, i) = cos * cp_from - sin * cp_to;
         }
 
-        matrix(to, i) = cos * cp_to + sin * cp_from;
+        matrix(s_row, i) = cos * cp_to + sin * cp_from;
     }
 }
 
-template <utils::FloatOrComplex T = long double>
-void GivensRightRotation(Matrix<T> &matrix, IndexType from, IndexType to,
-                         T first, T second) {
+template <utils::MutableMatrixType M>
+void GivensRightRotation(M &matrix, IndexType f_col, IndexType s_col,
+                         typename M::ElemType first, typename M::ElemType second) {
+    using T = typename M::ElemType;
     auto [cos, sin] = GetGivensCoefficients(first, second);
 
     for (IndexType i = 0; i < matrix.Rows(); ++i) {
-        auto cp_from = matrix(i, from);
-        auto cp_to = matrix(i, to);
+        auto cp_from = matrix(i, f_col);
+        auto cp_to = matrix(i, s_col);
 
         if constexpr (utils::details::IsFloatComplexT<T>::value) {
-            matrix(i, from) = std::conj(cos) * cp_from + std::conj(sin) * cp_to;
+            matrix(i, f_col) =
+                std::conj(cos) * cp_from - std::conj(sin) * cp_to;
         } else {
-            matrix(i, from) = cos * cp_from + sin * cp_to;
+            matrix(i, f_col) = cos * cp_from - sin * cp_to;
         }
 
-        if constexpr (utils::details::IsFloatComplexT<T>::value) {
-            matrix(i, to) = cos * cp_to - sin * cp_from;
-        } else {
-            matrix(i, to) = cos * cp_to - sin * cp_from;
-        }
+        matrix(i, s_col) = cos * cp_to + sin * cp_from;
     }
 }
 } // namespace matrix_lib::algorithms
