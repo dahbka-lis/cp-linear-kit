a270df2e45bccb85f81576609f28f42733c8c3e6
Daniil Timizhev <ditimizhev@edu.hse.ru>
2024-04-15 17:32:57 +0300

    fix zero elem

--- utils/sign.h
+++ utils/sign.h
@@ -6,13 +6,13 @@
 namespace matrix_lib::utils {
 template <utils::FloatOrComplex T = long double>
 T Sign(T value) {
-    if constexpr (utils::IsFloatComplexValue<T>()) {
+    if constexpr (utils::details::IsFloatComplexT<T>::value) {
         if (utils::IsZeroFloating(value)) {
             return T{0};
         }
         return value / std::sqrt(std::norm(value));
     } else {
-        return (value >= 0) ? T{1} : T{-1};
+        return (value >= T{0}) ? T{1} : T{-1};
     }
 }
 } // namespace matrix_lib::utils
