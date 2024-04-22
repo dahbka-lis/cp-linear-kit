80ed31c44b343f69b1e65b34dc6275d84768fca4
Daniil Timizhev <ditimizhev@edu.hse.ru>
2024-04-22 22:42:57 +0300

    real bidiagonalization for complex

--- algorithms/bidiagonalization.h
+++ algorithms/bidiagonalization.h
@@ -11,6 +11,26 @@ struct BidiagonalBasis {
     Matrix<T> B;
     Matrix<T> VT;
 };
+
+template <utils::MutableMatrixType F, utils::MutableMatrixType S>
+void RowToReal(F &B, S &U, IndexType idx) {
+    auto coeff = std::conj(B(idx, idx) / std::abs(B(idx, idx)));
+    auto B_row = B.GetRow(idx);
+    auto U_row = U.GetRow(idx);
+
+    B_row *= coeff;
+    U_row *= coeff;
+}
+
+template <utils::MutableMatrixType F, utils::MutableMatrixType S>
+void ColumnToReal(F &B, S &V, IndexType idx) {
+    auto coeff = std::conj(B(idx, idx + 1) / std::abs(B(idx, idx + 1)));
+    auto B_row = B.GetColumn(idx + 1);
+    auto V_col = V.GetColumn(idx + 1);
+
+    B_row *= coeff;
+    V_col *= coeff;
+}
 } // namespace details
 
 using IndexType = matrix_lib::details::Types::IndexType;
@@ -32,14 +52,21 @@ Bidiagonalize(const M &matrix) {
         HouseholderLeftReflection(B, col_reduction, col, col);
         HouseholderLeftReflection(U, col_reduction, col);
 
-        if (col + 2 < B.Columns()) {
-            Matrix<T> row_reduction =
-                B.GetSubmatrix({col, col + 1}, {col + 1, B.Columns()});
-            HouseholderReduction(row_reduction);
+        if constexpr (utils::details::IsFloatComplexT<T>::value)
+            details::RowToReal(B, U, col);
 
-            HouseholderRightReflection(B, row_reduction, col + 1, col);
-            HouseholderRightReflection(V, row_reduction, col + 1);
-        }
+        if (col + 1 >= B.Columns())
+            continue;
+
+        Matrix<T> row_reduction =
+            B.GetSubmatrix({col, col + 1}, {col + 1, B.Columns()});
+        HouseholderReduction(row_reduction);
+
+        HouseholderRightReflection(B, row_reduction, col + 1, col);
+        HouseholderRightReflection(V, row_reduction, col + 1);
+
+        if constexpr (utils::details::IsFloatComplexT<T>::value)
+            details::ColumnToReal(B, V, col);
     }
 
     U.Conjugate();
